3496ccb142cfa43d0b01bdbab99c283c9bd0c982
madking55 <madking55@gmail.com>
2019-04-11 09:49:09 +0200

    Finish Section Classes I

---
+++ Classes_I/superclass.rb
@@ -0,0 +1,17 @@
+p 5.class.ancestors
+p 5.class.superclass
+p 5.class.superclass.superclass
+p 5.class.superclass.superclass.superclass
+
+puts
+
+p 3.14.class.ancestors
+p 3.14.class.superclass
+p 3.14.class.superclass.superclass
+p 3.14.class.superclass.superclass.superclass
+
+puts
+
+p [1, 2, 3].class.ancestors
+p [1, 2, 3].class.superclass
+p [1, 2, 3].class.superclass.superclass
